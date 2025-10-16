791759292e46d5303e5d241c0062090bd4640756
Julie LE RAL <julie.le-ral@cpe.fr>
2025-10-16 16:52:23 +0200

    nouveau jet

--- TP4_casse_brique_code.py
+++ TP4_casse_brique_code.py
@@ -16,40 +16,35 @@ mafenetre = tk.Tk()
 mafenetre.title('Casse brique')
 mafenetre.geometry('1800x900')
 
-# Création des boutons quitter et lancer
-boutonquitter = Button(mafenetre, text = 'Quitter', bg = 'chocolate2', fg = 'white', width = 6, font = ("Britannic Bold", 16), command = mafenetre.destroy)
-boutonquitter.pack(side = 'right', padx = 10)
-boutonlancer = Button(mafenetre, text = 'Lancer', bg = 'royalblue2', fg = 'white', width = 6, font = ("Britannic Bold", 16))
-boutonlancer.pack(side = 'left', padx = 10)
-
 frame_haut = Frame(mafenetre)
 frame_haut.pack(side = 'top', fill = 'x')
 
 # Affichage du titre du jeu
 labeltitre = Label(frame_haut, text = 'CASSE BRIQUE', fg = 'Black', font = ("Britannic Bold", 30))
-labeltitre.pack(padx = 3, pady = 3)
-labeltitre.place(x = 500, y = 40)
+labeltitre.place(x = 650, y = 40)
 
 # Affichage du score et du nombre de vie
 score = StringVar()
 score.set("Score ->")
 labelscore = Label(frame_haut, textvariable = score, height = 2, width = 10, font = ("Calibri", 20))
 labelscore.pack(side = 'right', padx = 10, pady = 10)
+
 vie = StringVar()
 vie.set("Vie ->")
 labelvie = Label(frame_haut, textvariable = vie, height = 2, width = 10, font = ("Calibri", 20))
 labelvie.pack(side = 'left', padx = 10, pady = 10)
 
 # Création du cadre du jeu
-canvas = tk.Canvas(mafenetre, bg = 'black', width = 1200, height = 700)
-canvas.pack(fill = 'both')
+canvas = tk.Canvas(mafenetre, bg = 'black', width = 900, height = 700)
+canvas.pack(pady=20)
 
 
-class Brique() : 
+class brique() : 
+    briques = []
     def __init__(self, canvas, x, y, largeur, hauteur, couleur) :
-    # fonction qui permet d'initialiser les brique
-    # entrée : le canvas, la taille du cadre et la couleur
-    # sortie : les briques
+        # fonction qui permet d'initialiser les brique
+        # entrée : le canvas, la taille du cadre et la couleur
+        # sortie : les briques
         self.canvas = canvas
         self.x = x
         self.y = y
@@ -57,22 +52,23 @@ class Brique() :
         self.hauteur = hauteur
         self.couleur = couleur
         self.id = self.canvas.create_rectangle(x, y, x + largeur, y + hauteur, fill = couleur, outline = 'black')
+        brique.briques.append(self)
 
     def CreationBriques(canvas, lignes, couleur) :
     # fonction qui permet de créée les briques du jeux
     # entrée : le canvas, le nombre de ligne et la couleur
     # sortie : création des briques
-        nb_briques_par_ligne = 8
-        larg_brique = 170 #canvas_width // nb_briques_par_ligne
+        nb_briques_par_ligne = 7
+        larg_brique = 120 #canvas_width // nb_briques_par_ligne
         haut_brique = 50
-        debligne = 3 # nombre de pixels avant la première ligne
+        debligne = 5 # nombre de pixels avant la première ligne
         for x in range(lignes): 
-            row = debligne + (haut_brique * x)
+            row = debligne + (haut_brique + 5) * x
             for y in range(nb_briques_par_ligne) :
-                col = y * larg_brique
-                canvas.create_rectangle(col, row, col + larg_brique, row + haut_brique, fill = couleur, outline = 'black')
+                col = y * (larg_brique + 10)
+                brique(canvas, col, row, larg_brique - 5, haut_brique, couleur)
 
-Brique.CreationBriques(canvas, 7, "darkorchid4")
+brique.CreationBriques(canvas, 7, "darkorchid4")
 
 
 class raquette() : 
@@ -102,19 +98,19 @@ class raquette() :
     # entrée : 
     # sortie : la raquette bouge    
         coords = self.canvas.coords(self.raquette)
-        if coords[0] < 1250 :   
+        if coords[0] < 900 :   
             self.canvas.move(self.raquette, self.dx, 0)
 
 maraquette = raquette(canvas)
 
 
-largeur = 1340
+largeur = 900
 hauteur = 700
-rayon = 15
-x = largeur/2
-y = hauteur/2
-vitesse = rd.uniform (2, 2) * 5
-angle = rd.uniform(0 , 2 * mat.pi)
+rayon = 10
+x = largeur / 2
+y = hauteur / 2
+vitesse = 5
+angle = rd.uniform(0.3 , 2.8)
 dx = vitesse * mat.cos(angle)
 dy = vitesse * mat.sin(angle)
 
@@ -132,25 +128,25 @@ class ball() :
 balle = canvas.create_oval(x - rayon , y - rayon , x + rayon , y + rayon , width = 1, outline = 'white', fill = 'white')
 
 
-def collision() : 
-    a1x, a1y, a2x, a2y = canvas.bbox(maraquette)
+def collision() :
+    # fonction qui permet que la balle se déplace
+    # entrée : aucune
+    # sortie : la balle se déplace
+    global dx, dy, x, y, rayon
+    a1x, a1y, a2x, a2y = canvas.bbox(maraquette.raquette) 
     b1x, b1y, b2x, b2y = canvas.bbox(balle)
-    if a1x <= b1x <= a2x and a1y <= b1y <= a2y :
-
-    if a1x <= b2x <= a2x and a1y <= b2y <= a2y :
-
-    if a1x <= b1y <= a2y and a1y <= b1x <= a2x :
-
-    if a1x <= b1y <= a2y and a1y <= b1x <= a2x :
-
-
+    if (b2x >= a1x and b1x <= a2x) and (b2y >= a1y and b1y <= a2y) and dy > 0:
+        dy = -abs(dy)
+        y = a1y - rayon
+        canvas.coords(balle, x - rayon, y - rayon, x + rayon, y + rayon)
 
 
 def  deplacement() :
-# fonction qui permet que la balle se déplace
-# entrée : aucune
-# sortie : la balle se déplace
+    # fonction qui permet que la balle se déplace
+    # entrée : aucune
+    # sortie : la balle se déplace
     global x, y, dx, dy, rayon, largeur, hauteur
+    collision()
     if x + rayon + dx > largeur :
         x = 2 * (largeur - rayon ) - x
         dx = - dx
@@ -158,8 +154,8 @@ def  deplacement() :
         x = 2 * rayon - x
         dx = - dx
     if  y + rayon + dy > hauteur :
-        y = 2 * (hauteur - rayon ) - y
-        dy = - dy 
+        x, y = largeur / 2, hauteur / 2
+        dx, dy = 5 * mat.cos(angle), -abs(5 * mat.sin(angle))
     if  y - rayon + dy < 0 :
         y = 2 * rayon - y
         dy = - dy
@@ -170,11 +166,12 @@ def  deplacement() :
 
 deplacement()
 
+# Création des boutons quitter et lancer
+boutonquitter = Button(mafenetre, text = 'Quitter', bg = 'chocolate2', fg = 'white', height = 4, width = 7, font = ("Britannic Bold", 16), command = mafenetre.destroy)
+boutonquitter.pack(side = 'right', padx = 100)
 
-
-
-
-
+boutonlancer = Button(mafenetre, text = 'Lancer', bg = 'royalblue2', fg = 'white', height = 4, width = 7, font = ("Britannic Bold", 16))
+boutonlancer.pack(side = 'left', padx = 100)
 
 
 mafenetre.mainloop()
